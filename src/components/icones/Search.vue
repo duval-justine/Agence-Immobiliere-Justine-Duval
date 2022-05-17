d9c2b0284dcb3c4d1b30c40e64f73f9197a92c34
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-17 18:46:30 +0200

    Ajout icone Loupe

---
+++ src/components/icones/Search.vue
@@ -0,0 +1,26 @@
+<template>
+    <svg
+  width="20"
+  height="20"
+  viewBox="0 0 20 20"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class="flex-grow-0 flex-shrink-0 w-5 h-5 relative"
+  preserveAspectRatio="none"
+>
+  <path
+    d="M9.58341 17.5C13.9557 17.5 17.5001 13.9556 17.5001 9.58333C17.5001 5.21108 13.9557 1.66667 9.58341 1.66667C5.21116 1.66667 1.66675 5.21108 1.66675 9.58333C1.66675 13.9556 5.21116 17.5 9.58341 17.5Z"
+    stroke="white"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ></path>
+  <path
+    d="M18.3334 18.3333L16.6667 16.6667"
+    stroke="white"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    ></path>
+    </svg>
+</template>
