32cfb5ea7a17d002655a4136fba364262ff75bc3
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-05-17 19:06:16 +0200

    bord SearchList

--- src/components/SearchListe.vue
+++ src/components/SearchListe.vue
@@ -9,7 +9,7 @@ export default {
             <input 
             type="search"
             placeholder="Search with Search Bar"
-            class=" w-full h-12 px-4 border-2 rounded-lg border-indigo-100 placeholder:text-xs "/>
+            class=" w-full h-12 px-4 border-2 rounded-m border-indigo-100 placeholder:text-xs "/>
             <flechebas class="p-1 -ml-8"/>
         </div>
     
